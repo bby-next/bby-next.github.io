fdfb64af3bd04a2bdf1668d8b1fdd7f1d1c0594c
Kit Brown-Watts <kbwatts@gmail.com>
2016-09-04 17:07:39 -0700

    sample app

---
+++ sw-precache-config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  staticFileGlobs: [
+    '/index.html',
+    '/manifest.json',
+    '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
+    '/images'
+  ],
+  navigateFallback: '/index.html',
+  navigateFallbackWhitelist: [/^(?!.*\.html$|\/data\/).*/],
+  runtimeCaching: [
+    {
+      // urlPattern: /\/data\/images\/.*/,
+      urlPattern: /\/multimedia.bbycastatic.ca\/multimedia\//,
+      handler: 'cacheFirst',
+      options: {
+        cache: {
+          maxEntries: 200,
+          name: 'items-cache'
+        }
+      }
+    },
+    {
+      urlPattern: /\/data\/categories\/.*json/,
+      handler: 'fastest',
+      options: {
+        cache: {
+          maxEntries: 100,
+          name: 'data-cache'
+        }
+      }
+    },
+    {
+      urlPattern: /\/data\/i18n\/.*json/,
+      handler: 'fastest',
+      options: {
+        cache: {
+          maxEntries: 100,
+          name: 'data-cache'
+        }
+      }
+    }
+  ]
+};
